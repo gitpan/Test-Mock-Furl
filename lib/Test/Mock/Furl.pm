29ffde2b16fbb739c13bfdfe0e37879ffdeb1350
Dai Okabayashi <bayashi@cpan.org>
2013-06-07 16:15:44 +0000

    Import of BAYASHI/Test-Mock-Furl-0.04 from CPAN.
    gitpan-cpan-distribution: Test-Mock-Furl
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         BAYASHI/Test-Mock-Furl-0.04.tar.gz
    gitpan-cpan-author:       BAYASHI
    gitpan-cpan-maturity:     released

--- lib/Test/Mock/Furl.pm
+++ lib/Test/Mock/Furl.pm
@@ -10,7 +10,7 @@ our @EXPORT = qw/
     $Mock_furl_res $Mock_furl_resp $Mock_furl_response
 /;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 BEGIN {
     # Don't load the mock classes if the real ones are already loaded
--- t/00_compile.t
+++ t/00_compile.t
@@ -2,6 +2,8 @@ use strict;
 use Test::AllModules;
 
 BEGIN {
+    eval 'use Win32;1' if $^O && $^O =~ /Win32/;
+
     all_ok(
         search_path => 'Test::Mock::Furl',
         use_ok      => 1,
